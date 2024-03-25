e0005f20e66663fed82da9c7d0cbb096463f48df
darian <xxy.moming@gmail.com>
2024-03-25 10:03:49 +0800

    Initial Commit

---
+++ ObservableExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableExample
+//
+//  Created by Darian Mitchell  on 2024/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableExample/ObservableExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableExampleApp.swift
+//  ObservableExample
+//
+//  Created by Darian Mitchell  on 2024/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ObservableExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
