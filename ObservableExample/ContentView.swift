fde1c44120ad308862e74bcb494cb8cc0127e557
darian <xxy.moming@gmail.com>
2024-03-25 10:20:06 +0800

    works!

--- ObservableExample/ContentView.swift
+++ ObservableExample/ContentView.swift
@@ -10,23 +10,22 @@
 import SwiftUI
 
 struct ContentView: View {
-     private var book = Book()
+    // add @State
+     @State private var book = Book()
     
     var body: some View {
         VStack {
             Text(book.title)
-            // want to change the title of the book
-            // passing book.title produce errors:
-            // Cannot convert value of type 'String' to expected argument type 'Binding<String>'
-            // we need a binding!
-            TextField("title", text: book.title)
+            // book.title -> $book.title
+            TextField("title", text: $book.title)
         }
         .padding()
     }
 }
 
 
-
+// add @Observable
+@Observable
 class Book {
     var title = "swift programming"
 }
